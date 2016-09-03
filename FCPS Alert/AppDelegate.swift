2619a09420514f17e27ed494bf54313462ce9b8b
William Wright <admin@mac-admin.local>
2016-09-03 22:08:36 +0300

    added main view to swift 3 and will be adding firebase later

--- FCPS Alert/AppDelegate.swift
+++ FCPS Alert/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
 
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    fileprivate func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
     
